23b907d1f0f7e83e79ea7c8ff6e0269fba4f6c48
Vitaly Suhanov <vitalijs.suhanovs@gmail.com>
2014-12-05 00:44:18 +0200

    added karma support

---
+++ js/InputMask.directive.spec.js
@@ -0,0 +1,11 @@
+describe('inputMask directive', function ()
+{
+	beforeEach(module('inputMask'));
+
+	var template = '';
+
+	it('should be tested', function ()
+	{
+		expect(true).toBe(false);
+	})
+});
